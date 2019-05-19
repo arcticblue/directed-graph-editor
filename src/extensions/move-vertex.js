f078b2ba904296587242dd75aec258d45d56707f
Nils Olsson <nils.olsson@arcticblue.se>
2019-05-19 11:03:47 +0200

    Add move vertex implementation

--- src/extensions/move-vertex.js
+++ src/extensions/move-vertex.js
@@ -2,11 +2,41 @@ const moveVertex = (editor) => {
   const stage = editor.stage
   const graph = editor.graph
 
-  stage.on('mousedown', (event) => {})
+  let cursorPoint = {}
 
-  stage.on('mousemove', (event) => {})
+  const onMouseDown = (event) => {
+    const vertex = event.target.parent
+    if (vertex.isSelected) {
+      cursorPoint = { x: event.evt.x, y: event.evt.y }
+    }
+  }
 
-  stage.on('mouseup', (event) => {})
+  const onMouseMove = (event) => {
+    if (event.evt.which && Object.entries(cursorPoint).length !== 0) {
+      const point = { x: event.evt.x, y: event.evt.y }
+      const delta = { x: point.x - cursorPoint.x, y: point.y - cursorPoint.y }
+      editor.selected.forEach((vertex) => vertex.move(delta))
+      graph.batchDraw()
+      cursorPoint = point
+    }
+  }
+
+  const onMouseUp = (event) => {
+    cursorPoint = {}
+  }
+
+  graph.on('select', (vertex) => {
+    const node = vertex.node
+    node.on('mousedown.move', onMouseDown)
+    stage.on('mousemove.move', onMouseMove)
+    node.on('mouseup.move', onMouseUp)
+  })
+
+  graph.on('unselect', (vertex) => {
+    const node = vertex.node
+    node.off('.move')
+    stage.off('.move')
+  })
 }
 
 export default moveVertex
